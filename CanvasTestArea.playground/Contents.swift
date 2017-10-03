7564b9344370f191ade0b775cd9efdf4c4d8834e
Dylan Becker <dbecker@lcs-mba3263.lcs.on.ca>
2017-10-03 12:56:54 -0400

    Got the eyes sorted out!

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -23,7 +23,7 @@ import PlaygroundSupport
 // Create a new canvas
 let canvas = Canvas(width: 300, height: 500)
 
-// Draw eyes
+// Draw left eye
 //canvas.drawAxes()
 canvas.translate(byX: 25, byY: 300)
 canvas.rotate(by: -45)
@@ -32,16 +32,21 @@ canvas.drawEllipse(centreX: 50, centreY: 50, width: 60, height: 100)
 canvas.rotate(by: 45)
 canvas.drawAxes()
 
+//Draw right eye
 canvas.translate(byX: 200, byY: 0)
+canvas.drawAxes()
 canvas.rotate(by: 45)
 canvas.drawEllipse(centreX: 0, centreY: 0, width: 60, height: 100)
+canvas.rotate(by: -45)
 
 canvas.fillColor = Color.white
-canvas.translate(byX: 0, byY: 30)
+canvas.translate(byX: -8, byY: 20)
+canvas.drawAxes()
 canvas.drawEllipse(centreX: 0, centreY: 0, width: 15, height: 15)
 
 canvas.fillColor = Color.white
-canvas.translate(byX: -60, byY: 60)
+canvas.translate(byX: -105, byY: 0)
+canvas.drawAxes()
 canvas.drawEllipse(centreX: 0, centreY: 0, width: 15, height: 15)
 //// Draw a circle at the origin with radius of 50 pixels
 //canvas.drawEllipse(centreX: -50, centreY: 50, width: 50, height: 100)
