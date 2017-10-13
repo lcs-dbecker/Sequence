f553a12a3051f1d1c12a73c789c9590cb631cc42
Dylan Becker <dbecker@lcs-mba3263.lcs.on.ca>
2017-10-12 21:28:13 -0400

    did the nose lines( left and right)

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -64,9 +64,30 @@ canvas.drawEllipse(centreX: 135, centreY: -73, width: 40, height: 20)
 
 
 //Draw Nose
-canvas.drawAxes()
+
+
 canvas.fillColor = Color.black
 canvas.drawEllipse(centreX: 50, centreY: -60, width: 55, height: 30)
+
+// Move the axes back to the bottom left corner
+canvas.translate(byX: -112, byY: -320)
+canvas.drawAxes()
+
+
+
+// Nose Line left
+canvas.fillColor = Color.black
+canvas.drawEllipse(centreX: 133, centreY: 222, width: 53, height: 53)
+canvas.borderColor = Color.white
+canvas.fillColor = Color.white
+canvas.drawEllipse(centreX: 131, centreY: 227 , width: 51, height: 46)
+
+//Nose line right
+canvas.fillColor = Color.black
+canvas.drawEllipse(centreX: 185, centreY: 222, width: 53, height: 53)
+canvas.borderColor = Color.white
+canvas.fillColor = Color.white
+canvas.drawEllipse(centreX: 187, centreY: 227 , width: 51, height: 46)
 
 
 
